dfe4cbf839e13cb431f8387cc8310da542e5ffe4
Jernej Strasner <jernej.strasner@gmail.com>
2017-12-09 13:33:57 -0800

    Moved over from another project

--- Sources/UInt256/UInt256.swift
+++ Sources/UInt256/UInt256.swift
@@ -1,3 +1,296 @@
-struct UInt256 {
-    var text = "Hello, World!"
+//
+//  UInt256.swift
+//  BitcoinMiner
+//
+//  Created by Jernej Strasner on 11/24/17.
+//  Copyright © 2017 Jernej Strasner. All rights reserved.
+//
+
+import Foundation
+import Accelerate
+
+public struct UInt256 {
+
+    private var value = vU256()
+
+    public init() {}
+
+    public init(_ num: UInt64) {
+        value.s.LSW = UInt32(truncatingIfNeeded: num)
+        value.s.d7 = UInt32(truncatingIfNeeded: num >> 32)
+    }
+
+    public init(_ num: UInt32) {
+        value.s.LSW = num
+    }
+
+    public init(hex: String) {
+        precondition(hex.count != 0, "Empty string")
+        precondition(hex.count <= 64, "String is too large")
+        precondition(hex.count % 2 == 0, "A hexadecimal string must have a power of 2 length")
+        let processedHex = repeatElement("0", count: 64 - hex.count) + hex // left padding
+        for c in processedHex {
+            guard let number = UInt32(String(c), radix: 16) else {
+                fatalError("Invalid hex character!")
+            }
+            // Shift left before adding
+            var shifted = vU256()
+            vLL256Shift(&value, 4, &shifted)
+            // Add to shifted value
+            var addition = vU256()
+            addition.s.LSW = number
+            vU256Add(&shifted, &addition, &value)
+        }
+    }
+
+    public static func <<(lhs: UInt256, rhs: UInt32) -> UInt256 {
+        var num = lhs.value
+        var result = vU256()
+        vLL256Shift(&num, rhs, &result)
+        var int = UInt256()
+        int.value = result
+        return int
+    }
+
+    private init(_ value: vU256) {
+        self.value = value
+    }
+
+}
+
+extension UInt256 : ExpressibleByIntegerLiteral {
+
+    public typealias IntegerLiteralType = UInt64
+
+    public init(integerLiteral value: IntegerLiteralType) {
+        self.init(value)
+    }
+
+}
+
+extension UInt256 : ExpressibleByStringLiteral {
+
+    public typealias StringLiteralType = String
+
+    public init(stringLiteral value: StringLiteralType) {
+        self.init(hex: value)
+    }
+
+}
+
+extension UInt256 /*: BinaryInteger*/ {
+
+    static var isSigned: Bool {
+        return false
+    }
+
+    init?<T>(exactly source: T) where T : BinaryFloatingPoint {
+        // TODO: proper floating point type initialization
+        if let i = UInt64(exactly: source) {
+            self.init(i)
+        }
+        return nil
+    }
+
+    init<T>(_ source: T) where T : BinaryFloatingPoint {
+        // TODO: proper floating point type initialization
+        self.init(UInt64(source))
+    }
+
+    init<T>(truncatingIfNeeded source: T) where T : BinaryInteger {
+        // TODO: proper integer type initialization
+        self.init(UInt64(truncatingIfNeeded: source))
+    }
+
+    init<T>(clamping source: T) where T : BinaryInteger {
+        // TODO: proper integer type initialization
+        self.init(UInt64(clamping: source))
+    }
+
+    typealias Words = UInt256
+
+    var words: Words {
+        return self
+    }
+
+    var bitWidth: Int {
+        return 256
+    }
+
+    var trailingZeroBitCount: Int {
+        var c = 0
+        for b in self.reversed() {
+            if b.trailingZeroBitCount == 0 {
+                return c
+            }
+            c += b.trailingZeroBitCount
+        }
+        return c
+    }
+
+    public static func /(lhs: UInt256, rhs: UInt256) -> UInt256 {
+        var left = lhs.value
+        var right = rhs.value
+        var result = vU256()
+        vU256Divide(&left, &right, &result, nil)
+        return UInt256(result)
+    }
+
+    public static func /=(lhs: inout UInt256, rhs: UInt256) {
+        var right = rhs.value
+        var result = vU256()
+        vU256Divide(&lhs.value, &right, &result, nil)
+        lhs.value = result
+    }
+
+    public static func %(lhs: UInt256, rhs: UInt256) -> UInt256 {
+        var left = lhs.value
+        var right = rhs.value
+        var result = vU256()
+        vU256Mod(&left, &right, &result)
+        return UInt256(result)
+    }
+
+    public static func %=(lhs: inout UInt256, rhs: UInt256) {
+        var right = rhs.value
+        var result = vU256()
+        vU256Mod(&lhs.value, &right, &result)
+        lhs.value = result
+    }
+
+    public static func +(lhs: UInt256, rhs: UInt256) -> UInt256 {
+        var left = lhs.value
+        var right = rhs.value
+        var result = vU256()
+        vU256Add(&left, &right, &result)
+        return UInt256(result)
+    }
+
+    public static func +=(lhs: inout UInt256, rhs: UInt256) {
+        var right = rhs.value
+        var result = vU256()
+        vU256Add(&lhs.value, &right, &result)
+        lhs.value = result
+    }
+
+}
+
+extension UInt256 : Comparable {
+
+    public static func ==(lhs: UInt256, rhs: UInt256) -> Bool {
+        for (lv, rv) in zip(lhs, rhs) {
+            if lv != rv {
+                return false
+            }
+        }
+        return true
+    }
+
+    public static func <(lhs: UInt256, rhs: UInt256) -> Bool {
+        for (lv, rv) in zip(lhs, rhs) {
+            if lv < rv {
+                return true
+            }
+            if lv > rv {
+                return false
+            }
+        }
+        return false
+    }
+
+}
+
+extension UInt256 : Collection {
+
+    public typealias Element = UInt32
+
+    public var startIndex: Int {
+        return 0
+    }
+
+    public var endIndex: Int {
+        return 8
+    }
+
+    public subscript(index: Int) -> Element {
+        get {
+            switch index {
+            case 0: return value.s.MSW
+            case 1: return value.s.d2
+            case 2: return value.s.d3
+            case 3: return value.s.d4
+            case 4: return value.s.d5
+            case 5: return value.s.d6
+            case 6: return value.s.d7
+            case 7: return value.s.LSW
+            default: fatalError("Index out of bounds")
+            }
+        }
+        set {
+            switch index {
+            case 0: value.s.MSW = newValue
+            case 1: value.s.d2 = newValue
+            case 2: value.s.d3 = newValue
+            case 3: value.s.d4 = newValue
+            case 4: value.s.d5 = newValue
+            case 5: value.s.d6 = newValue
+            case 6: value.s.d7 = newValue
+            case 7: value.s.LSW = newValue
+            default: fatalError("Index out of bounds")
+            }
+        }
+    }
+
+    public func index(after i: Int) -> Int {
+        precondition(i < endIndex, "Can't advance beyond endIndex")
+        return i + 1
+    }
+
+}
+
+extension UInt256 : CustomDebugStringConvertible {
+
+    public var debugDescription: String {
+        return self.map{ String($0) }.joined(separator: "_")
+    }
+
+    public var hexDescription: String {
+        return self.map({
+            let s = String($0, radix: 16)
+            return repeatElement("0", count: 8 - s.count) + s
+        }).joined()
+    }
+
+}
+
+extension UInt32 {
+
+    public init(_ i: UInt256) {
+        for x in i {
+            if x > 0 {
+                self.init(x)
+                return
+            }
+        }
+        self.init(0)
+    }
+
+}
+
+extension UInt64 {
+
+    public init(_ i: UInt256) {
+        for (y, x) in i.enumerated() {
+            if x > 0 {
+                if y+1 < i.endIndex {
+                    self.init(UInt64(x) << 32 + UInt64(i[y+1]))
+                } else {
+                    self.init(x)
+                }
+                return
+            }
+        }
+        self.init(0)
+    }
+
 }
--- Tests/UInt256Tests/UInt256Tests.swift
+++ Tests/UInt256Tests/UInt256Tests.swift
@@ -1,16 +1,79 @@
+//
+//  BitcoinMinerTests.swift
+//  BitcoinMinerTests
+//
+//  Created by Jernej Strasner on 11/25/17.
+//  Copyright © 2017 Jernej Strasner. All rights reserved.
+//
+
 import XCTest
-@testable import UInt256
+import UInt256
 
 class UInt256Tests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(UInt256().text, "Hello, World!")
+    
+    func testComparison() {
+        let a = UInt256(UInt64.max)
+        let b = UInt256(UInt64.max-1)
+        XCTAssertTrue(a > b)
+        XCTAssertTrue(a != b)
+        XCTAssertTrue(b < a)
+        XCTAssertTrue(a == UInt256(UInt64.max))
+        XCTAssertTrue(b == UInt256(UInt64.max-1))
+    }
+    
+    func testComparisonSwapped() {
+        let a = UInt256(UInt64.max-1)
+        let b = UInt256(UInt64.max)
+        XCTAssertTrue(a < b)
+        XCTAssertTrue(a != b)
+        XCTAssertTrue(b > a)
+        XCTAssertTrue(a == UInt256(UInt64.max-1))
+        XCTAssertTrue(b == UInt256(UInt64.max))
     }
 
+    func testComparisonLarge() {
+        let a = UInt256(UInt32(1)) << 224
+        let aT = UInt256(UInt32(1)) << 225
+        XCTAssertTrue(a < aT)
+        let b = UInt256(hex: "0000000000000000e067a478024addfecdc93628978aa52d91fabd4292982a50")
+        let bT = UInt256(hex: "00000000000000015f5300000000000000000000000000000000000000000000")
+        XCTAssertTrue(b < bT)
+    }
 
-    static var allTests = [
-        ("testExample", testExample),
-    ]
+    func testBitShiftLeft() {
+        let a = UInt256(UInt32.max) << 32
+        let b = UInt256(UInt64.max ^ UInt64(UInt32.max))
+        XCTAssertTrue(a == b)
+    }
+
+    func testConvertToUInt32() {
+        let a = UInt256(UInt32.max)
+        XCTAssertTrue(UInt32(a) == UInt32.max)
+        let b = UInt256(UInt32.max) << 128
+        XCTAssertTrue(UInt32(b) == UInt32.max)
+    }
+
+    func testConvertToUInt64() {
+        let a = UInt256(UInt64.max)
+        XCTAssertTrue(UInt64(a) == UInt64.max)
+        let b = UInt256(UInt32.max) << 32
+        XCTAssertTrue(UInt64(b) == (UInt64.max ^ UInt64(UInt32.max)))
+    }
+
+    func testHexParsing() {
+        let a = UInt256(hex: "53058b35")
+        let b = UInt256(UInt64(0x53058b35))
+        XCTAssertTrue(a == b)
+    }
+
+    func testIntegerLiteral() {
+        let a: UInt256 = 728
+        XCTAssertTrue(a == UInt256(UInt64(728)))
+    }
+
+    func testStringLiteral() {
+        let a: UInt256 = "ffffffff"
+        XCTAssertTrue(a == UInt256(UInt32(0xffffffff)))
+    }
+
 }
